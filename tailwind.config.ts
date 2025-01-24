01fddb0fda9005f2fe3760576518b50769861ffa
ayyanovia1121 <noviayya1121@gmail.com>
2025-01-24 14:39:06 +0700

    Tailwind confic Customize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        blurwhite: "rgba(255,255,255,0.7)",
+        blurblue: "rgba(9, 151, 124, 0.1)",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        poppins : ["Poppins", "sans-serif"],
+      },
+    },
+    // custome screens
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
