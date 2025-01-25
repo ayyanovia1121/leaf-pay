8e165a6e20d653fa44b66fc12a2f1e2232fde4f0
ayyanovia1121 <noviayya1121@gmail.com>
2025-01-25 12:59:47 +0700

    Navbar Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <Navbar />
+      <Hero />
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,58 @@
+'use client';
 import React from "react";
 import Image from "next/image";
-import logo from "../assets/logo.svg";
+import { logo, menu, close } from "../assets";
 import Link from "next/link";
 import { navLinks } from "../constants";
-const Navbar = () => {
+import { useState } from "react";
 
+const Navbar = () => {
+const [togle, setTogle] = useState(false);
   return (
-    console.log(navLinks),
     <div className="flex justify-center items-center sm:px-16 px-6">
       <div className="xl:max-[1280px] w-full">
         <nav className="w-full flex py-6 justify-between items-center">
           <Image src={logo} alt="logo" className="w-[128px] h-[32px]" />
           <div className="sm:flex hidden justify-end items-center flex-1">
-            {navLinks.map((navItem) => (
-              <Link
-                key={navItem.id}
-                href={navItem.link}
-                className={`font-poppins`}
-              >
-                <span className=" text-white text-sm !cursor-pointer mr-10">{navItem.title}</span>
+            {navLinks.map((navItem, index) => (
+              <Link key={navItem.id} href={navItem.link}>
+                <span
+                  className={`text-white font-poppins font-normal text-sm ${
+                    index === navLinks.length - 1 ? "mr-0" : "mr-10"
+                  } !cursor-pointer`}
+                >
+                  {navItem.title}
+                </span>
               </Link>
             ))}
+          </div>
+
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+            <Image
+              src={togle ? close : menu}
+              alt="menu"
+              className="w-[28px] h-[28px] object-contain"
+              onClick={() => setTogle((prev) => !prev)}
+            />
+            <div
+              className={`${
+                togle ? "flex" : "hidden"
+              } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+            >
+              <div className="flex flex-col justify-end items-center flex-1">
+                {navLinks.map((navItem, index) => (
+                  <Link key={navItem.id} href={navItem.link}>
+                    <span
+                      className={`text-white font-poppins font-normal text-sm ${
+                        index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                      } !cursor-pointer`}
+                    >
+                      {navItem.title}
+                    </span>
+                  </Link>
+                ))}
+              </div>
+            </div>
           </div>
         </nav>
       </div>
